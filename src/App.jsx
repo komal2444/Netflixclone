e488f9d6dabfa55536f6d8bcea0b03664039da01
Komal <komalkadam1553@gmail.com>
2025-06-14 15:26:15 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from "./pages/Home/Home";
-import Login from "./pages/login/Login";
+import Login from './pages/login/Login'
 import Player from "./pages/Player/Player";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from './firebase';
